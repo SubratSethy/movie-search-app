42605eb3741115775323c56f8e72e9abcfee68a8
SubratSethy <151069317+SubratSethy@users.noreply.github.com>
2024-08-07 20:15:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,8 +22,10 @@ getMovies(APIURL);
 //It is the init call or initial call while page get loaded for the first time i.e, it will show the popular movie page. 
 // showMovies(data) , here we pass the data that fetched from api to showmovies fuction and this take the data show the result in the form of cards in the ui.
 
+//
 const showMovies = (data) => {
     moiveBox.innerHTML = "";
+    //after each function call of getMovies make movieBox blank so that at every new search function call new result got shown i.e; result not get appened with previous result.  
     data.results.forEach(
         (result) => {
             const imagePath = result.poster_path === null ? "img/image-missing.png" : IMGPATH + result.poster_path;
@@ -37,7 +39,9 @@ const showMovies = (data) => {
             // </div>
             // `
             const box = document.createElement("div")
+            //here, div named element is created but it is not a part of the web page.
             box.classList.add("box")
+            //This line adds a CSS class named "box" to the div element.
             box.innerHTML = `
                 <img src="${imagePath}" alt="" />
                 <div class="overlay">
@@ -52,6 +56,9 @@ const showMovies = (data) => {
                  </div>
             `
             moiveBox.appendChild(box)
+            //The box element is added to the moiveBox element as a child, making it part of the DOM within that container,making it visible on the page.
+            //here, all the data got fetched from api and added to div named element and at last it get appened with movie Box element.
+            
         }
     )
 }
@@ -61,8 +68,18 @@ document.querySelector("#search").addEventListener(
     function (event) {
         if (event.target.value != "") {
             getMovies(SEARCHAPI + event.target.value)
+            // "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="; at last value that user type in search box got added getMovies function call every time.
+            // search movies
+       
+            
         } else {
             getMovies(APIURL);
+            //popular movies
         }
     }
 )
+//if we do console.log("event ") it will show that event that took place if user press a,b,c,d,e,f,g,h or any key that will show there.
+// if we do console.log(event.target) it will show that where event that took place i.e; at search box.
+//if we do console.log(event.target.vaule) it will show that what usaer type in the search box.
+
+
